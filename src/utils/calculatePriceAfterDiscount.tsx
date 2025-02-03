4a4af13b3b7ecfad091390e154ac32e49e47e5a8
Geegne <geegnebab@gmail.com>
2025-02-04 01:36:33 +0200

    fixed calculatePriceAfterDiscount util function, now it displays the right price after the discount

--- src/utils/calculatePriceAfterDiscount.tsx
+++ src/utils/calculatePriceAfterDiscount.tsx
@@ -4,7 +4,7 @@ type Props = {
 }
 
 function calculatePriceAfterDiscount({ price = 0, discount = 0 }: Props) {
-  return Math.trunc(price - (price * 100 / discount));
+  return Math.trunc(price - (price * discount / 100));
 }
 
 export default calculatePriceAfterDiscount;
