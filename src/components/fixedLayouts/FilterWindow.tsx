34154751dffe1c69e4703ab49a3abc97264770af
Geegne <geegnebab@gmail.com>
2025-02-22 08:02:08 +0200

    imporved the UI of FilterWindow component by making it scrollable and the close, selected filters elements are grouped and turned into sticky top-0 to ensure easier navigation and control

--- src/components/fixedLayouts/FilterWindow.tsx
+++ src/components/fixedLayouts/FilterWindow.tsx
@@ -38,83 +38,92 @@ export default function FilterWindow () {
   return (
     <div
       className={`
-        fixed top-0 left-0 
-        flex flex-col w-full h-full bg-[var(--shade-v3-color)] z-[2000]
+        fixed top-0 left-0 pb-8
+        flex flex-col w-screen h-screen bg-[var(--shade-v3-color)] z-[2000]
         transition-all duration-300 ease-in-out
-        backdrop-blur-[2px]
+        backdrop-blur-[2px] overflow-y-scroll
         ${toggle ? 'translate-y-[0%]' : 'translate-y-[100%]'}
       `}
     >
       <hr className="border-inbetween"/>
-      <button
+      <div
         className="
-          relative flex items-center justify-end w-full p-2 opacity-100 cursor-pointer
-          w-full text-body text-3xl font-thin
-          flex items-center justify-center gap-2 bg-[hsla(0,0%,80%,0.6)]           
+          sticky top-0 z-[5]
         "
-        onClick={handleClick}
-        data-type="close_button_is_clicked"
       >
-        <ArrowUp 
-          className="w-6 h-6 text-between rotate-[180deg] rounded-full border-solid border-body-light border-[1px] p-1"
-        />
-        <h2>
-          CLOSE
-        </h2>
-      </button>
-      <hr className="border-inbetween"/>
-      <section
-        className="flex gap-8 p-4 w-[700px] mx-auto"
-      >
-        {selectedCategories.length > 0 &&
-          <ul
-            className="flex flex-row flex-wrap gap-2"
-          >
-            {selectedCategories?.map((itm: any, i: number) =>
-              <li
-                className="
-                  flex items-center text-sm text-heading-invert 
-                  bg-heading gap-2 px-2 py-1 cursor-pointer
-                  hover:opacity-80
-                  transition-all duraiton-200 ease-in-out
-                "
-                role="button"
-                data-type="clear_filter_button_is_clicked"
-                data-section-key={itm.key}
-                key={i}
-                onClick={handleClick}
-              >
-                <span>
-                  {itm.title}
-                </span>
-                <span
-                  className="text-xs border border-heading-invert px-[0.3rem] rounded-full"
-                >
-                  x
-                </span>
-              </li>
-            )}
-          </ul>
-        }
         <button
-          className={`
-            shrink-0 underline text-base text-heading whitespace-nowrap
-            ${selectedCategories.length > 0 ? 'hover:font-bold cursor-pointer' : 'hover:font-normal cursor-default'}
-            transition-all duration-200 ease-in-out  
-          `}
-          style={{ textDecoration: selectedCategories.length > 0 ? 'underline' : 'none'}}
-          data-type="clear_all_filters_button_is_clicked"
+          className="
+            flex items-center justify-end w-full p-5 opacity-100 cursor-pointer
+            w-full text-body text-3xl font-thin
+            flex items-center justify-center gap-2 bg-[hsla(0,0%,80%,0.6)] 
+            backdrop-blur-[3px] z-[5]
+          "
           onClick={handleClick}
+          data-type="close_button_is_clicked"
         >
-          {selectedCategories.length > 0 
-           ? 'Clear all filters'
-           : 'No Filters are selected'
-          }
+          <ArrowUp 
+            className="w-6 h-6 text-between rotate-[180deg] rounded-full border-solid border-body-light border-[1px] p-1"
+          />
+          <h2>
+            CLOSE
+          </h2>
         </button>
-      </section>
-      <hr className="border-inbetween mx-4 md:w-[668px] md:mx-auto"/>
+        <hr className="border-inbetween"/>
+        <section
+          className="
+            flex gap-8 p-4 w-full max-w-[700px] mx-auto backdrop-blur-[3px]
+          "
+        >
+          {selectedCategories.length > 0 &&
+            <ul
+              className="flex flex-row flex-wrap gap-2"
+            >
+              {selectedCategories?.map((itm: any, i: number) =>
+                <li
+                  className="
+                    flex items-center text-sm text-heading-invert 
+                    bg-heading gap-2 px-2 py-1 cursor-pointer
+                    hover:opacity-80
+                    transition-all duraiton-200 ease-in-out
+                  "
+                  role="button"
+                  data-type="clear_filter_button_is_clicked"
+                  data-section-key={itm.key}
+                  key={i}
+                  onClick={handleClick}
+                >
+                  <span>
+                    {itm.title}
+                  </span>
+                  <span
+                    className="text-xs border border-heading-invert px-[0.3rem] rounded-full"
+                  >
+                    x
+                  </span>
+                </li>
+              )}
+            </ul>
+          }
+          <button
+            className={`
+              shrink-0 underline text-base text-heading whitespace-nowrap
+              ${selectedCategories.length > 0 ? 'hover:font-bold cursor-pointer' : 'hover:font-normal cursor-default'}
+              transition-all duration-200 ease-in-out  
+            `}
+            style={{ textDecoration: selectedCategories.length > 0 ? 'underline' : 'none'}}
+            data-type="clear_all_filters_button_is_clicked"
+            onClick={handleClick}
+          >
+            {selectedCategories.length > 0 
+             ? 'Clear all filters'
+             : 'No Filters are selected'
+            }
+          </button>
+        </section>
+        <hr className="border-inbetween mx-4 md:w-[668px] md:mx-auto"/>
+      </div>
       <section
-        className="divide-y divide-inbetween px-4 w-full md:w-[700px] mx-auto"
+        className="divide-y divide-inbetween px-4 w-full max-w-[700px] mx-auto"
       >
         <FilterExpandWrapper 
           sectionName="SORT"
