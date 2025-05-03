c0c30f1afbb3ab46e0d1bde0a57a1a8fb876c86a
Geegne <geegnebab@gmail.com>
2025-05-03 04:11:33 +0300

    working on themes tab, finsihed with the get request and its implementations, after that the post request should be implemented

--- src/app/[lang]/admin/edit-themes/Table.tsx
+++ src/app/[lang]/admin/edit-themes/Table.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useQuery } from '@tanstack/react-query';
 
 // COMPONENTS
+import LoadingTable from '@/components/LoadingTable'
 import LineMdLink from '@/components/svgs/LineMdLink';
 import TablerCopy from '@/components/svgs/TablerCopy';
 import MaterialSymbolsCheckRounded from '@/components/svgs/MaterialSymbolsCheckRounded';
@@ -34,11 +35,13 @@ export default function Table() {
   const setAlertToggle = useAlertMessageStore((state) => state.setToggle);
   const setAlertType = useAlertMessageStore((state) => state.setType);
   const setAlertMessage = useAlertMessageStore((state) => state.setMessage);
+  const loadingArray = [1, 2, 3, 4];
 
-  const { data, error, isLoading } = useQuery({
+  const { data: currentTheme, error, isLoading } = useQuery({
     queryKey: ['themes'],
     queryFn: getThemes,
-  })
+  });
+  const isSameTheme = (id: number) => id === currentTheme?.scheme_id;
 
   const checkToggle = (toggle: boolean, hookIndex: number | string, refIndex: number | string) => {
     if (toggle === true && hookIndex === refIndex) return true;
@@ -72,118 +75,8 @@ export default function Table() {
   // DEBUG & UI
   // console.log('themes data: ', data);
 
-  if (!isLoading) return (
-    <div className="flex flex-col gap-4 overflow-x-auto">
-      <h3
-        className="sticky left-0 text-lg text-transparent"
-      >
-        {isEn ? 'Schemes' : 'سكيمات'}
-      </h3>
-      <table
-        className="
-          min-w-full overflow-hidden 
-          divide-y divide-underline bg-white rounded-lg whitespace-nowrap
-        "
-      >
-        <thead className="text-body">
-          <tr>
-            <th scope="col" className={`px-6 py-3 font-medium ${isEn ? 'text-left' : 'text-right'} text-xs font-medium tracking-wider`}>
-              {isEn ? 'NAME' : 'الاسم'}
-            </th>
-            <th scope="col" className={`px-6 py-3 font-medium ${isEn ? 'text-left' : 'text-right'} text-xs font-medium tracking-wider`}>
-              {isEn ? 'Pallete' : 'لوح الألوان'}
-            </th>
-            <th scope="col" className={`px-6 py-3 font-medium ${isEn ? 'text-left' : 'text-right'} text-xs font-medium tracking-wider`}>
-              {isEn ? 'OPTIONS' : 'الخيارات'}
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-underline">
-          {themePallets.map((itm, i) => 
-            <tr 
-              key={i}
-              className="hover:bg-yellow-50 transition-all duration-300 ease-in-out"
-            >
-              {/* <td className="px-6 py-4 text-heading">{itm.name[isEn ? 'en' : 'ar']}</td> */}
-              <td className="px-6 py-4 text-heading">{itm.name[isEn ? 'en' : 'ar']}</td>
-              <td className={`flex px-6 py-4 text-sm text-body`}>
-                <ul className="flex flex-row w rounded-md overflow-hidden">
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.primary_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.secondary_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.content_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.content_inbetween_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.inbetween_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.content_invert_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.primary_invert_color}}
-                  />
-                  <li
-                    className={`p-3`}
-                    style={{backgroundColor: itm.secondary_invert_color}}
-                  />
-                </ul>
-              </td>
-              <td className="px-6">
-                <div className="flex gap-2">
-                  <button 
-                    className={`
-                      relative bg-background-light rounded-md
-                      transition-all duration-500 ease-in-out
-                      ${true
-                        ? 'bg-green-400' 
-                        : 'bg-background-light'
-                      }
-                    `}
-                  >
-                    <SolarGalleryBold 
-                      className={`
-                        w-7 h-7 p-1 rounded-md cursor-pointer 
-                        transition-all duration-200 ease-in-out
-                        ${true
-                          ? 'invisible opacity-0'
-                          : 'visible opacity-100' 
-                        }
-                      `}
-                    />    
-                    <SolarGalleryCheckBold 
-                      className={`
-                        absolute top-1/2 left-1/2
-                        translate-x-[-50%] translate-y-[-50%]
-                        w-7 h-7 p-1 rounded-md cursor-pointer 
-                        transition-all duration-200 ease-in-out
-                        ${true 
-                          ? 'visible opacity-100' 
-                          : 'invisible opacity-0'
-                        }
-                      `}
-                    />    
-                  </button>
-                </div>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+  if (isLoading) return (
+    <LoadingTable />
   )
 
   return (
@@ -216,12 +109,15 @@ export default function Table() {
           {themePallets.map((itm, i) => 
             <tr 
               key={i}
-              className="hover:bg-yellow-50 transition-all duration-300 ease-in-out"
+              className={`
+                hover:bg-yellow-50 transition-all duration-300 ease-in-out
+                ${isSameTheme(itm.scheme_id) ? 'bg-green-100' : 'bg-transparent'}
+              `}
             >
               {/* <td className="px-6 py-4 text-heading">{itm.name[isEn ? 'en' : 'ar']}</td> */}
               <td className="px-6 py-4 text-heading">{itm.name[isEn ? 'en' : 'ar']}</td>
               <td className={`flex px-6 py-4 text-sm text-body`}>
-                <ul className="flex flex-row w rounded-md overflow-hidden">
+                <ul className="flex flex-row w rounded-md overflow-hidden border-solid border-inbetween border-2">
                   <li
                     className={`p-3`}
                     style={{backgroundColor: itm.primary_color}}
@@ -262,7 +158,7 @@ export default function Table() {
                     className={`
                       relative bg-background-light rounded-md
                       transition-all duration-500 ease-in-out
-                      ${true
+                      ${isSameTheme(itm.scheme_id)
                         ? 'bg-green-400' 
                         : 'bg-background-light'
                       }
@@ -272,7 +168,7 @@ export default function Table() {
                       className={`
                         w-7 h-7 p-1 rounded-md cursor-pointer 
                         transition-all duration-200 ease-in-out
-                        ${true
+                        ${isSameTheme(itm.scheme_id)
                           ? 'invisible opacity-0'
                           : 'visible opacity-100' 
                         }
@@ -284,7 +180,7 @@ export default function Table() {
                         translate-x-[-50%] translate-y-[-50%]
                         w-7 h-7 p-1 rounded-md cursor-pointer 
                         transition-all duration-200 ease-in-out
-                        ${true 
+                        ${isSameTheme(itm.scheme_id)
                           ? 'visible opacity-100' 
                           : 'invisible opacity-0'
                         }
---
+++ src/components/LoadingTable.tsx
@@ -0,0 +1,60 @@
+export default function LoadingTable () {
+  const loadingArray = [1, 2, 3, 4];
+  return (
+    <div className="flex flex-col gap-4 overflow-x-auto">
+      <h3
+        className="--opacity-blink w-[6rem] h-4 rounded-lg sticky left-0 text-lg text-transparent bg-[var(--background-deep-light-color)]"
+      >
+        Schemes
+      </h3>
+      <table
+        className="
+          --opacity-blink min-w-full overflow-hidden 
+          divide-y divide-underline bg-[var(--background-deep-light-color)] rounded-lg whitespace-nowrap
+        "
+      >
+        <thead className="text-body">
+          <tr>
+            <th scope="col" className={`opacity-0 px-6 py-3 font-medium text-xs font-medium tracking-wider`}>
+              NAME
+            </th>
+            <th scope="col" className={`opacity-0 px-6 py-3 font-medium text-xs font-medium tracking-wider`}>
+              Pallete
+            </th>
+            <th scope="col" className={`opacity-0 px-6 py-3 font-medium text-xs font-medium tracking-wider`}>
+              OPTIONS
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-underline">
+          {loadingArray.map((itm, i) => 
+            <tr 
+              key={i}
+              className="hover:bg-yellow-50 transition-all duration-300 ease-in-out"
+            >
+              <td className="opacity-0 px-6 py-4 text-heading">Some Name</td>
+              <td className={`flex px-6 py-4 text-sm text-body`}>
+                <ul className="flex flex-row w rounded-md overflow-hidden">
+                  <li
+                    className={`p-3`}
+                  />
+                </ul>
+              </td>
+              <td className="px-6">
+                <div className="flex gap-2">
+                  <button 
+                    className={`
+                      relative bg-background-light rounded-md opacity-0
+                      transition-all duration-500 ease-in-out
+                    `}
+                  >
+                  </button>
+                </div>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
