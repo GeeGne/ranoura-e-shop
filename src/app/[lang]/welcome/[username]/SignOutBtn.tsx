18ad0cb718d999116d2617c0d92d9cf7a349cae1
Geegne <geegnebab@gmail.com>
2025-06-06 00:44:19 +0300

    implemented signout feature by adjusting SignOutBtn component

--- src/app/[lang]/welcome/[username]/SignOutBtn.tsx
+++ src/app/[lang]/welcome/[username]/SignOutBtn.tsx
@@ -1,4 +1,16 @@
+// HOOKS
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+// COMPONENTS
 import BtnA from '@/components/BtnA';
+import SvgSpinnersRingResize from '@/components/svgs/activity/SvgSpinnersRingResize';
+
+// STORES
+import { useAlertMessageStore, useLanguageStore } from '@/stores/index';
+
+// API
+import signoutUser from '@/lib/api/auth/signout/post';
 
 type Props = {
   className?: string;
@@ -6,23 +18,80 @@ type Props = {
 } & React.ComponentPropsWithRef<"div"> ;
 
 export default function SignOutBtn ({ className, isLoading, ...props }: Props) {
+
+  const queryClient = useQueryClient();
+  const lang = useLanguageStore(state => state.lang);
+  const isEn = lang === 'en';
+
+  const setAlertToggle = useAlertMessageStore((state) => state.setToggle);
+  const setAlertType = useAlertMessageStore((state) => state.setType);
+  const setAlertMessage = useAlertMessageStore((state) => state.setMessage);
+
+  const [ isMutating, setIsMutating ] = useState<boolean>(false);
+
+  const signoutUserMutation = useMutation({
+    mutationFn: signoutUser,
+    onSettled: () => {
+      setIsMutating(false);
+    },
+    onMutate: () => {
+      setIsMutating(true);
+    },
+    onSuccess: (data) => {
+      setAlertToggle(Date.now());
+      setAlertType("success");
+      setAlertMessage(data.message[isEn ? 'en' : 'ar']);
+      window.location.reload();
+    },
+    onError: (data) => {
+      setAlertToggle(Date.now());
+      setAlertType("error");
+      setAlertMessage(data.message);
+      window.location.reload();
+    }
+  })
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { type } = e.currentTarget.dataset;
+
+    switch (type) {
+      case 'signout_button_is_clicked':
+        console.log('clicked!');
+        signoutUserMutation.mutate();
+        break;
+      default:
+        console.error('Unknown type: ', type); 
+    }
+  }
+
   return (
     <div    
       className={`
         ${className}
       `}
+      data-type="signout_button_is_clicked"
+      onClick={handleClick}
       { ...props }
     >
       <BtnA
         className={`
-          px-4 py-2 text-md font-bold rounded-md
+          relative px-4 py-2 text-md font-bold rounded-md
           ${isLoading 
             ? '--opacity-blink bg-background-deep-light text-background-deep-light' 
             : 'bg-primary text-heading-invert'
           }
         `}
       >
-        Signout
+        <span className={`${isMutating ? 'opacity-0' : 'opacity-100'}`}>
+          Signout
+        </span>
+        <SvgSpinnersRingResize 
+          className={`
+            absolute top-1/2 left-1/2
+            translate-x-[-50%] translate-y-[-50%]
+            ${isMutating ? 'opacity-100' : 'opacity-0'}
+          `}
+        />
       </BtnA>
     </div>
   )
