433db2b9185459613c32c278786952568d88e557
Geegne <geegnebab@gmail.com>
2026-02-21 04:54:38 +0200

    wokring on StatusMap component

--- src/app/[lang]/welcome/[username]/orders/StatusMap.tsx
+++ src/app/[lang]/welcome/[username]/orders/StatusMap.tsx
@@ -1,21 +1,29 @@
+// COMPONENTS
 import LineMdConfirmCircleFilled from '@/components/svgs/LineMdConfirmCircleFilled';
+import LineMdCircleFilled from '@/components/svgs/LineMdCircleFilled';
+
+// JSON
+import STATUS_TRANSLATIONS from '@/json/translate/STATUS_TRANSLATIONS.json';
+
+// LIB
+import getTranslation from '@/utils/getTranslation';
 
 export default function StatusMap () {
-  const status = "confirmed";
-  
-  if (status === "confirmed") return (
-    <div className="relative">
+  const status: string = "SHIPPED";
+
+  if (status === "CONFIRMED") return (
+    <div className="flex flex-col w-full text-transparent items-center gap-2">
       <section 
-        className="relative w-full bg-transparent text-transparent h-[50px]"
+        className="relative flex w-[calc(100%-1rem)]"
       >
-        f
         <div 
           className="
             absolute top-1/2 translate-y-[-50%] left-0 w-[calc(100%-1rem)] h-2 bg-green-500
           "
         />
-        <ul className="
-            absolute top-0 left-0 w-full h-full
+        <ul 
+          className="
+            w-full h-full
             flex justify-between items-center
           "
         >
@@ -44,13 +52,12 @@ export default function StatusMap () {
             />
           </li>
         </ul>
-      </section>
-      <section 
+      <div 
         className="
-          --status-mask-ani absolute top-0 left-0 w-full text-transparent h-[50px] brightness-[150%]
+          --status-mask-ani absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]
+          w-full text-transparent h-[50px] brightness-[150%]
         "
       >
-        f
         <div 
           className="
             absolute top-1/2 translate-y-[-50%] left-0 w-[calc(100%-1rem)] h-2 bg-green-500
@@ -86,7 +93,144 @@ export default function StatusMap () {
             />
           </li>
         </ul>
+      </div>
       </section>
+      <ul className="flex justify-between w-full">
+        <span 
+          className="text-inbetween font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'PENDING').toUpperCase()}
+        </span>
+        <span 
+          className="text-inbetween font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'CONFIRMED').toUpperCase()}
+        </span>
+        <span 
+          className="text-inbetween font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'SHIPPED').toUpperCase()}
+        </span>
+        <span 
+          className="text-heading font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'DELIVERED').toUpperCase()}
+        </span>
+      </ul>
+    </div>
+  )
+
+  if (status === "SHIPPED") return (
+    <div className="flex flex-col w-full text-transparent items-center gap-2">
+      <section 
+        className="relative flex w-[calc(100%-1rem)]"
+      >
+        <div 
+          className="
+            absolute top-1/2 translate-y-[-50%] left-0 w-[calc(100%-1rem)] h-2 bg-[oklch(70.4%0.14182.503)]
+          "
+        />
+        <ul 
+          className="
+            w-full h-full
+            flex justify-between items-center z-[10]
+          "
+        >
+          <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+            1
+            <LineMdConfirmCircleFilled 
+              className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+            />
+          </li>
+          <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+            2
+            <LineMdConfirmCircleFilled 
+              className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+            />
+          </li>
+          <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+            3
+            <LineMdConfirmCircleFilled 
+              className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+            />
+          </li>
+          <li className="relative w-[50px] h-[50px] rounded-full bg-inbetween">
+            4
+            <LineMdCircleFilled 
+              className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+            />
+          </li>
+        </ul>
+        <div 
+          className="
+            absolute top-1/2 translate-y-[-50%] right-0 w-[33.33%] h-2 bg-inbetween z-[5]
+          "
+        />
+        <div 
+          className="
+            --status-mask-ani absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]
+            w-full text-transparent h-[50px] brightness-[150%]
+          "
+        >
+          <div 
+            className="
+              absolute top-1/2 translate-y-[-50%] left-0 w-[calc(100%-1rem)] h-2 bg-[oklch(70.4%0.14182.503)]
+            "
+          />
+          <ul className="
+              absolute top-0 left-0 w-full h-full
+              flex justify-between items-center z-[20]
+            "
+          >
+            <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+              1
+              <LineMdConfirmCircleFilled 
+                className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+              />
+            </li>
+            <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+              2
+              <LineMdConfirmCircleFilled 
+                className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+              />
+            </li>
+            <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+              3
+              <LineMdConfirmCircleFilled 
+                className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+              />
+            </li>
+            <li className="relative w-[50px] h-[50px] rounded-full bg-[oklch(70.4%0.14182.503)]">
+              4
+              <LineMdCircleFilled 
+                className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-12 h-12 text-background" 
+              />
+            </li>
+          </ul>
+        </div>
+      </section>
+      <ul className="flex justify-between w-full">
+        <span 
+          className="text-inbetween font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'PENDING').toUpperCase()}
+        </span>
+        <span 
+          className="text-inbetween font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'CONFIRMED').toUpperCase()}
+        </span>
+        <span 
+          className="text-heading font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'SHIPPED').toUpperCase()}
+        </span>
+        <span 
+          className="text-inbetween font-bold" 
+        >
+          {getTranslation(STATUS_TRANSLATIONS, 'DELIVERED').toUpperCase()}
+        </span>
+      </ul>
     </div>
   )
 }
---
+++ src/components/svgs/LineMdCircleFilled.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export default function LineMdCircleFilled(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillOpacity={0}
+        stroke="currentColor"
+        strokeDasharray={60}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M3 12c0 -4.97 4.03 -9 9 -9c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9Z"
+      >
+        <animate
+          fill="freeze"
+          attributeName="stroke-dashoffset"
+          dur="0.6s"
+          values="60;0"
+        ></animate>
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="0.6s"
+          dur="0.4s"
+          to={1}
+        ></animate>
+      </path>
+    </svg>
+  );
+}
